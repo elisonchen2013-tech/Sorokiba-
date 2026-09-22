6b7dfcdc2c43c084fe35c10441d61240367e1a80
Elison Chen <elisonchen2013@gmail.com>
2026-09-22 20:38:46 -0300

    Remover lógica de horário real do carrossel

--- home-carousel-v3.js
+++ home-carousel-v3.js
@@ -221,20 +221,6 @@
      HORÁRIO
   ========================================================= */
 
-  function getPeriod() {
-
-    const hour = new Date().getHours();
-
-    if (hour >= 5 && hour < 12) {
-      return 'morning';
-    }
-
-    if (hour >= 12 && hour < 18) {
-      return 'afternoon';
-    }
-
-    return 'night';
-  }
 
   /* =========================================================
      ESTILOS
